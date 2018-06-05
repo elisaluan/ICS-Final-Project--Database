9274c86389125bfaedaaa610b8eac13473e42e85
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2018-06-05 13:58:56 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,2 +1,103 @@
-public class User {
+
+
+public abstract class User {
+    String firstName;
+    String lastName;
+    String userID;
+    String password;
+    Date birthDate;
+
+    public User (String givenFirstName, String givenLastName, String givenID, String password, int birthYear,
+                 int birthDay,int birthMonth)
+    {
+        birthDate = new Date (birthDay, birthMonth, birthYear);
+        firstName = givenFirstName;
+        lastName = givenLastName;
+        userID = givenID;
+        this.password = password;
+
+    }
+
+    //Accessors and Mutators
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLasName(String lasName) {
+        this.lastName = lasName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    //Methods
+
+    public int compareToAge (User other)
+    {
+        if (birthDate.getYear() > other.birthDate.getYear())
+            return -1;
+        else if (birthDate.getYear() < other.birthDate.getYear())
+            return 1;
+        else if (birthDate.getMonth() < other.birthDate.getMonth())
+            return 1;
+        else if (birthDate.getMonth() > other.birthDate.getMonth())
+            return -1;
+        else if (birthDate.getDay() < other.birthDate.getDay())
+            return 1;
+        else if (birthDate.getDay() > other.birthDate.getDay())
+            return -1;
+        else
+            return 0;
+
+    }
+
+    public int compareToFirstName (User other)
+    {
+        return (this.getFirstName().compareToIgnoreCase(other.getFirstName()));
+    }
+
+    public int  compareToLastName (User other)
+    {
+        return (this.getLastName().compareToIgnoreCase(other.getLastName()));
+    }
+
+    public String toString ()
+    {
+        String output;
+        output = "Full Name: " + getFirstName() + " " + getLastName() + "\n";
+        output = output + "User ID: " + getUserID() + "\n";
+        output = output + "Birth Date: " + getBirthDate();
+        return output;
+    }
+
+
 }
