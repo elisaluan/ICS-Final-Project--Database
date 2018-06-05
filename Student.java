c561ca0712a7b7d04df0c247b4db9c5e873b8534
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2018-06-05 13:31:04 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,13 +6,14 @@ public class Student extends User {
     //add transcript class and
 
     public Student (String givenFirstName, String givenLastName, String givenID, String password, int birthYear,
-                    int birthDay,int birthMonth, boolean isEsl, boolean isInternationalStudent, int grade)
+                    int birthDay,int birthMonth, boolean isEsl, boolean isInternationalStudent, boolean isIEP, int grade)
     {
         super (givenFirstName, givenLastName, givenID, password, birthYear, birthDay,
                 birthMonth);
         eslStudent = isEsl;
         internationalStudent = isInternationalStudent;
         this.grade = grade;
+        IEP = isIEP
 
         }
 
