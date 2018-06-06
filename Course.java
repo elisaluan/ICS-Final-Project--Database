789e84516a50af10d687023df897fbfd261c2a05
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2018-06-06 15:12:54 -0400

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Course {
+
+    //Initializes the fields
+    private Teacher teacher;
+    private String courseCode;
+    private CourseStatistics average;
+    private ArrayList <MyCourse> studentInCourseList = new ArrayList<MyCourse>();
+    private boolean complete;
+
+    //Constructor
+    public Course (Teacher assignedTeacher, String assignedCourseCode, ArrayList <Student> listOfStudents,
+                   boolean courseComplete )
+    {
+        teacher = assignedTeacher;
+        courseCode = assignedCourseCode;
+        complete = courseComplete;
+        
+        
+
+    }
+
+    //Methods
+    public void addStudentToCourse (Student other)
+    {
+        studentInCourseList.add(/*student my course */);
+    }
 }
