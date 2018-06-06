9ee97829a0c9e4731a09d61b5c22a2c13215d80f
winter420 <kirill.shmakov@student.tdsb.on.ca>
2018-06-06 14:31:49 -0400

    Update CreditSummary.java

--- CreditSummary.java
+++ CreditSummary.java
@@ -1,2 +1,11 @@
+import java.util.*;
+
 public class CreditSummary {
+  public static void main(String[] args){
+    ArrayList<CoursesByYear> yearList = new ArrayList<CoursesByYear>();
+  }
+  public String toString(){
+    String summary;
+    summary +=
+  }
 }
