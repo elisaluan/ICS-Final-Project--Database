60762afeb0e22101ea623b5a78c79bf03ba08878
ahmedrizvi <39914275+ahmedrizvi@users.noreply.github.com>
2018-06-06 14:30:06 -0400

    Update Date.java

--- Date.java
+++ Date.java
@@ -3,6 +3,7 @@ public class Date {
     int month;
     int year;
 
+    //constructor
     public Date (int day, int month, int year)
     {
         this.day = day;
@@ -35,4 +36,19 @@ public class Date {
     public void setYear(int year) {
         this.year = year;
     }
-}
+    
+    //comparing two date objects
+    //if current date is earlier than comparing date, 1 will be returned
+    //if current date is later than comparing date, -1 will be returned
+    //if both dates are the same, 0 will be returned
+    public compareToDay int (Date current, Date comparing){
+        if ( (current.year < comparing.year) || (current.year == comparing.year && current.month < comparing.month) || (current.year==comparing.year && current.month==comparing.month && current.day){
+            return 1;
+        } else if(current.year > comparing.year){
+            return -1;
+        }
+    }//compareToDay method
+    public toString String(Date formatDate){
+        return (formatDate.day + formatDate.month + formatDate.year);
+    }
+}//Date class
