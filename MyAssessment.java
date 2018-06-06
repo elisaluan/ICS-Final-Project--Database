231ef4a0a287e197b19df915668cd6d4221993a7
winter420 <kirill.shmakov@student.tdsb.on.ca>
2018-06-06 14:48:23 -0400

    Update MyAssessment.java

--- MyAssessment.java
+++ MyAssessment.java
@@ -1,2 +1,20 @@
 public class MyAssessment {
+  //Initializes fields
+  private String assessmentName;
+  private double assessmentMarkPer;
+  private double assessmentNum;
+  private double assessmentDen;
+  private double weight;
+  
+  //Method to calculate the mark
+  public void calcMark(){
+    assessmentMarkPer = assessmentNum/assessmentDen;
+  }
+  
+  //Method to return all info as a string
+  public String toString(){
+    String s = "Assessment name: " + assessmentName + "\nAssessment percent mark: " + assessmentMarkPer;
+    s+= "\nAssessment mark: " + assessmentNum + "/" + assessmentDen + "\nWeight: " + weight;
+    return s;
+  }
 }
