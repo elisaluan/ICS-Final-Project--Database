28217f22f74f678b038f594eb4bd29a1dcda1cbb
michaelTanson <39774935+michaelTanson@users.noreply.github.com>
2018-06-07 12:50:07 -0400

    Add files via upload
    added in constructor, accessors, mutators. filled in fields

--- Student.java
+++ Student.java
@@ -1,2 +1,81 @@
+import java.util.*;
+
 public class Student {
+	
+	// FIELDS
+	
+	private int numCourses;
+	private creditSummary transcript;
+	private int grade;
+	private boolean esl;
+	private boolean internationalStudent;
+	private boolean iep;
+	
+//////////////////////////////////////////////////////////////////
+	
+	// CONSTRUCTOR
+	
+	public Student(int courses, creditSummary transcript, int level, boolean esl, boolean international, boolean iep)
+	{
+		numCourses = courses;
+		this.transcript = transcript;
+		grade = level;
+		this.esl = esl;
+		internationalStudent =  international;
+		this.iep = iep;
+	}
+
+/////////////////////////////////////////////////////////////////
+	
+	// ACCESSORS
+	
+	public int getNumCourse()
+	{
+		return numCourses;
+	}
+	
+	public creditSummary getTranscript()
+	{
+		return transcript;
+	}
+	
+	public int getGrade()
+	{
+		return grade;
+	}
+	
+	public boolean getESL()
+	{
+		return esl;
+	}
+	
+	public boolean getInternationalStudent()
+	{
+		return internationalStudent;
+	}
+	
+	public boolean getIEP()
+	{
+		return iep;
+	}
+	
+///////////////////////////////////////////////////////////////////
+	
+	// MUTATORS
+	
+	public void setNumCourse (int num)
+	{
+		numCourses = num;
+	}
+	
+	public void setTranscript (creditSummary transcript)
+	{
+		this.transcript = transcript;
+	}
+	
+	
+	
+	
+	
+	
 }
