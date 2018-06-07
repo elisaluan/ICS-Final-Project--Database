64c73a986fc1deac66062eaed1b91027ce677466
ahmedrizvi <39914275+ahmedrizvi@users.noreply.github.com>
2018-06-07 13:23:31 -0400

    Update CourseStatistics.java

--- CourseStatistics.java
+++ CourseStatistics.java
@@ -1,10 +1,3 @@
 public class CourseStatistics{
-  private double classMean;
-  private double classMedian;
-  private String courseCode;
-  
-  //constructor
-  public CourseStatistics (Course course, ){
-  }
   
 }//CourseStatistics class
