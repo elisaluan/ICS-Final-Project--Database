c215e03ef27429553eb4cdeb94eebcb833f03065
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2018-06-07 13:12:16 -0400

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,2 +1,12 @@
-public class Teacher {
+import java.util.ArrayList;
+
+public class Teacher extends User {
+    //Initializes Fields
+    private static final int NUMCOURSES = 3;
+    private String [] coursesTeaching = new String [NUMCOURSES];
+    private ArrayList <Course> courseArrayList = new ArrayList <Course> ();
+    private Date beginTeaching;
+    
+    
+
 }
