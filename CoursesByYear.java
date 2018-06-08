ea1630194064aa30fead3fb7f51881997b76164b
winter420 <kirill.shmakov@student.tdsb.on.ca>
2018-06-08 14:53:16 -0400

    Update CoursesByYear.java

--- CoursesByYear.java
+++ CoursesByYear.java
@@ -1,2 +1,8 @@
 public class CoursesByYear {
+  final int NUM_SEMESTERS = 2;
+  CoursesBySemester[] semesterList = new CoursesBySemester[NUM_SEMESTERS];
+    
+  public String toString();
+  
+  }
 }
