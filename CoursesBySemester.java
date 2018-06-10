7d20ea5574d075d160798bad6b0929264e8a35a7
michaelTanson <39774935+michaelTanson@users.noreply.github.com>
2018-06-10 16:47:54 -0400

    Add files via upload
    just uploading everything cause I fixed a few things like syntax in different other classes and I can't remember which anymore. This also  has the CoursesBySemester, CoursesByYear, CreditSummary, Student, and CourseStatistics finished

--- CoursesBySemester.java
+++ CoursesBySemester.java
@@ -1,2 +1,37 @@
+import java.util.*;
+
 public class CoursesBySemester {
+	
+	// FIELDS
+
+	private ArrayList<MyCourse> courseList = new ArrayList<MyCourse>();
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// CONSTRUCTOR
+	
+	public CoursesBySemester(ArrayList<MyCourse> courses)
+	{
+		int length = courses.size();
+		
+		for (int i = 0 ; i < length ; i++)
+		{
+			courseList.add(courses.get(i));
+		}
+	}
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// METHODS
+	
+	public String toString()
+	{
+		String output = "";
+		for(int i = 0 ; i < courseList.size() ; i++)
+		{
+			output += courseList.get(i).getCourseCode() + " - " + courseList.get(i).getCurrentAverage() + "\n";
+		}
+		return output;
+	}
+	
 }
