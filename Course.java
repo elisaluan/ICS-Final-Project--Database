f9672c88351f49809c9abfb1e966ef12388a6629
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2018-06-13 04:02:35 -0400

    Update Course.java

--- Course.java
+++ Course.java
@@ -26,6 +26,49 @@ public class Course {
         }
 
     }
+    
+    //Accessors
+    public ArrayList<MyCourse> getStudentInCourseList() {
+        return studentInCourseList;
+    }
+
+    public CourseStatistics getAverage() {
+        return average;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+    
+    //Mutators
+    public void setAverage(CourseStatistics average) {
+        this.average = average;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public void setStudentInCourseList(ArrayList<MyCourse> studentInCourseList) {
+        this.studentInCourseList = studentInCourseList;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+    
 
     //Methods
     public void addStudentToCourse (Student other)
