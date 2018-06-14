294f6a9c2a846e2af2a42b489dcce9e4bf79c10f
elisaluan <30943907+elisaluan@users.noreply.github.com>
2018-06-14 14:57:34 -0400

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,37 +1,30 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class Teacher extends User {
-	
-	// FIELDS
-	
-    private static final int NUMCOURSES = 3;
-    private String [] coursesTeaching = new String [NUMCOURSES];
-    private ArrayList <Course> courseArrayList = new ArrayList <Course> ();
-    private Date beginTeaching;
-    
-////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    // CONSTRUCTORS
-    
-    public Teacher()
-    {
-    	
-    }
-    
-////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    // ACCESSORS
-    
-    
-////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    // MUTATORS
-    
-    
-////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    // METHODS
-    
-    
-
+public class Teacher {
+   private final int MAX_COURSES = 3;
+   private int numTeaching;
+   private ArrayList<String> coursesTeaching = new ArrayList<String> ();
+   private Date beginTeach;
+   
+   // accessors
+   public int getNumTeaching () {
+      return numTeaching;
+   } 
+   public Arraylist<String> getCoursesTeaching () {
+      return coursesTeaching;
+   }
+   public Date getBeginTeach () {
+      return beginTeach;
+   }
+   
+   // mutators
+   public void setNumTeaching (int numTeaching) {
+      this.numTeaching = numTeaching;
+   }
+   public void setCoursesTeaching (ArrayList<String> coursesTeaching) {
+      this.coursesTeaching = coursesTeaching;
+   }
+   public void setBeginTeach(Date beginTeach) {
+      this.beginTeach = beginTeach;
+   }
 }
