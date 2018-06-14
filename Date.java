8599863e10c738028fdbcc8119983a7d4102ef3a
michaelTanson <Tanson-michaeltanson@ymail.com>
2018-06-14 19:09:31 -0400

    update

--- Date.java
+++ Date.java
@@ -1,56 +1,56 @@
 public class Date {
-    private int day;
-    private int month;
-    private int year;
-
-    //constructor
-    public Date (int day, int month, int year)
-    {
-        this.day = day;
-        this.month = month;
-        this.year = year;
-    }
-
-    //Accessors
-    public int getDay() {
-        return day;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    //Mutators
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-    
-    //comparing two date objects
-    //if current date is earlier than comparing date, 1 will be returned
-    //if current date is later than comparing date, -1 will be returned
-    //if both dates are the same, 0 will be returned
-    public int compareToDay (Date current, Date comparing){
-        if ( (current.year < comparing.year) || (current.year == comparing.year && current.month < comparing.month) || (current.year==comparing.year && current.month==comparing.month && current.day < comparing.day) ){
-            return 1;
-        } 
-        else if(( comparing.year < current.year) || (comparing.year == current.year && comparing.month < current.month) || (comparing.year==current.year && comparing.month==current.month && comparing.day < current.day) ){
-            return -1;
-        }else{
-            return 0;   
-        }
-    }//compareToDay method
+	private int day;
+	   private int month;
+	   private int year;
+	   
+	   // accessors
+	   public int getDay () {
+	      return day;
+	   }
+	   public int getMonth () {
+	      return month;
+	   }
+	   public int getYear () {
+	      return year;
+	   }
+	   
+	   // mutators
+	   public void setDay (int day) {
+	      if (day >= 1 && day <= 31) { // constant
+	         this.day = day;
+	      }
+	   }
+	   public void setMonth (int month) { // constant
+	      if (month >= 1 &&  month <= 12) {
+	         this.month = month;
+	      }
+	   }
+	   public void setYear (int year) { // constant
+	      this.year = year;
+	   }
+	   
+	   // methods
+	   public int compareToDate (Date other) {
+	      if (this.year > other.year) {
+	         return 1;
+	      } else if (this.year < other.year) {
+	         return -1;
+	      } else {
+	         if (this.month > other.month) {
+	            return 1;
+	         } else if (this.month < other.month){
+	            return -1;   
+	         } else {
+	            if (this.day > other.day) {
+	               return 1;
+	            } else if (this.day < other.day) {
+	               return -1;
+	            } else {
+	               return 0;
+	            } // if structure compares day
+	         } // if structure compares month
+	      } // if structure compares year 
+	   } // compareToDay method
     
     //converts the called date from int to all in one string  (DDMMYYYY) date format
     public String toString(){
--- School.java
+++ School.java
@@ -39,6 +39,25 @@ public class School {
     public String getSchoolName() {
         return schoolName;
     }
+    
+    public ArrayList<Student> getListOfStudents(){
+    	return listOfStudents;
+    }
+    
+    public ArrayList<Teacher> getListOfTeachers()
+    {
+    	return listOfTeachers;
+    }
+    
+    public ArrayList<GuidanceCounsellor> getListOfGuidanceCouns()
+    {
+    	return listOfGuidanceCouns;
+    }
+    
+    public Date getDateEstablished()
+    {
+    	return dateEstablished;
+    }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -68,40 +87,207 @@ public class School {
     
     // METHODS
     
+    // Selection sort to sort through ArrayList of students alphabetically
     public void sortStudentLastAZ()
     {
-    	int length = listOfStudents.size();
-    	String stud1 = "";
-    	String stud2 = "";
-    	String stud3 = "";
+    	int length = listOfStudents.size(); // length of the ArrayList
+
     	
     	for(int i = 0 ; i < length-1 ; i++)
     	{
-    		int idx = i;
-    		for(int j = 0 ; j < length ; j++)
+        	Student stud1 = null; // set stud1 and stud 2 to null after each pass
+        	Student stud2 = null; // optional
+        	
+    		int idx = i; //to use  for swapping, makes swapping ineffective if kept as i
+    		for(int j = i+1 ; j < length ; j++)
     		{
-    			stud1 = listOfStudents.get(j).getLastName();
-    			stud2 = listOfStudents.get(idx).getLastName();
-    			stud3 = listOfStudents.get(i).getLastName();
+    			stud1 = listOfStudents.get(j); // initializes stud1-3 as students from  the ArrayList
+    			stud2 = listOfStudents.get(idx);
     			
-    			if(stud1.compareTo(stud2) < 0)
+    			if(stud1.getLastName().compareTo(stud2.getLastName()) < 0) // compares the last name of students
     			{
-    				idx = j;
+    				idx = j; // changes idx to make swap effective
     			}
-    			else if(stud1.compareTo(stud2) == 0)
+    			else if(stud1.getLastName().compareTo(stud2.getLastName()) == 0) //compares the first name if the students have the same last name
     			{
-    				
+    				if(stud1.getFirstName().compareTo(stud2.getFirstName()) < 0)
+    				{
+    					idx = j; // chages idx to make swap effective
+    				}
     			}
     		}
-    		String temp = stud1;
-    		listOfStudents.get(idx).setLasName(stud3);
-    		listOfStudents.get(i).setLasName(temp);
-    		
+    	
+    		// swapping
+    		// only effective if  idx  becomes j
+    		Student temp = stud2;
+    		stud2 = listOfStudents.get(i);
+    		listOfStudents.set(i, temp);
     	}
     }
+    
+    public void sortStudentMarkHL()
+    {
+    	int length = listOfStudents.size(); // to get length of ArrayList
+    	boolean sorted = false; // initialization of boolean to check if array is sorted
+    	
+    	for(int i = 0 ; i < length && sorted == false ; i++)
+    	{
+    		
+    		for(int j = 0 ; j < length-i-1; j++)
+    		{
+    			// intializes two students to avoid repetitive method calling
+    			Student stud1 = listOfStudents.get(j);
+    			Student stud2 = listOfStudents.get(j+1);
+    			
+    			if(stud1.getTranscript().getYearList().get(stud1.getTranscript().getYearList().size()-1).calcYearAverage()
+    					< stud2.getTranscript().getYearList().get(stud1.getTranscript().getYearList().size()-1).calcYearAverage())
+    			{
+    				// set sorted to false to allow the method another pass on the array
+    				sorted = false;
+    				
+    				//swaps the two students
+    				listOfStudents.set(j, stud2);
+    				listOfStudents.set(j+1, stud1);
+    			}
+    		}
+    	}
+    }
+    
+    public void sortStudentMarkLH()
+    {
+    	int length = listOfStudents.size(); // to get length of ArrayList
+    	boolean sorted = false; // initialization of boolean to check if array is sorted
+    	
+    	for(int i = 0 ; i < length && sorted == false ; i++)
+    	{
+    		
+    		for(int j = 0 ; j < length-i-1; j++)
+    		{
+    			// intializes two students to avoid repetitive method calling
+    			Student stud1 = listOfStudents.get(j);
+    			Student stud2 = listOfStudents.get(j+1);
+    			
+    			if(stud1.getTranscript().getYearList().get(stud1.getTranscript().getYearList().size()-1).calcYearAverage()
+    					> stud2.getTranscript().getYearList().get(stud1.getTranscript().getYearList().size()-1).calcYearAverage())
+    			{
+    				// set sorted to false to allow the method another pass on the array
+    				sorted = false;
+    				
+    				//swaps the two students
+    				listOfStudents.set(j, stud2);
+    				listOfStudents.set(j+1, stud1);
+    			}
+    		
+    		}
+    	}
+    }
+    
+    // sorts the ArrayList of students by their Age using their birthdays
+    public void sortStudentAgeLH()
+    {
+    	int length = listOfStudents.size(); // to get length of ArrayList
+    	boolean sorted  = false; // initialization of boolean to check if ArrayList is sorted
+    	
+    	for(int i = 0 ; i > length - i - 1 && sorted == false ; i++) // outer for loop  to keep track of remaining passes
+    	{
+    		sorted = true;
+    		
+    		for(int j = 0 ; j < length - i - 1 ; j++) // inner loop to check and swap correspondingly until the outer loop
+    		{
+    			Student stud1 = listOfStudents.get(j); // initializes new variables to avoid repetitive method calling
+    			Student stud2 = listOfStudents.get(j+1);
+    			
+    			Date date1 = stud1.getBirthDate(); // initializes a new date to avoid repetitive method calling
+    			Date date2 = stud2.getBirthDate();
+    			
+    			if(date1.compareToDate(date2) > 0) // checks which one is older in termms of  year
+    			{
+    				sorted = false;								//<--- makes sorted false to enable one more pass in the outer loop
+    				
+    				listOfStudents.set(j, stud2);				//<--- swapping students
+    				listOfStudents.set(j+1, stud1);				//^^^^
+    			}
+    		}
+    	}
+    }
+    
+    // sorts the students by age from oldest to youngest using bubble sort
+    public void sortStudentAgeHL()
+    {
+    	int length = listOfStudents.size(); // to get length of ArrayList
+    	boolean sorted  = false; // initialization of boolean to check if ArrayList is sorted
+    	
+    	for(int i = 0 ; i > length - i - 1 && sorted == false ; i++) // outer for loop  to keep track of remaining passes
+    	{
+    		sorted = true;
+    		
+    		for(int j = 0 ; j < length - i - 1 ; j++) // inner loop to check and swap correspondingly until the outer loop
+    		{
+    			Student stud1 = listOfStudents.get(j); // initializes new variables to avoid repetitive method calling
+    			Student stud2 = listOfStudents.get(j+1);
+    			
+    			Date date1 = stud1.getBirthDate(); // initializes a new date to avoid repetitive method calling
+    			Date date2 = stud2.getBirthDate();
+    			
+    			if(date1.compareToDate(date2) < 0) {
+    				sorted = false;
+    				
+    				listOfStudents.set(j, stud2);
+    				listOfStudents.set(j+1, stud1);
+    			}
+    		}
+    	}
+    }
+    
+    // sorting method to sort teacher alphabetically using bubble sort
+    public void sortTeachersAlpha()
+    {
+    	int length = listOfTeachers.size();
+    	boolean sorted = false;
+    	
+    	for(int i = 0 ; i < length && sorted == false ; i++) //outer loop for the bubble sort
+    	{
+    		sorted = true; //controls if one more  pass SHOULD be made
+    		for(int j = 0 ; j < length - i - 1 ; j++)
+    		{
+    			Teacher teach1 = listOfTeachers.get(j); // initalizes new variables to avoid  repetitive method calling
+    			Teacher teach2 = listOfTeachers.get(j+1);
+    			
+    			if(teach1.getLastName().compareTo(teach2.getLastName()) < 0) //compares the teachers last name
+    			{
+    				sorted = false;								//turned false to allow one more pass in the outer loop 
+    				
+    				listOfTeachers.set(j, teach2);				//swapping students
+    				listOfTeachers.set(j+1, teach1);			//^^^^
+    			}
+    			else if(teach1.getLastName().compareTo(teach2.getLastName()) == 0)
+    			{
+    				if(teach1.getFirstName().compareTo(teach2.getFirstName()) < 0) // compares the teachers first name if they have the same last name
+    				{
+    					sorted = false;							//turned false to allow one more pass in the outer loop 
+    					
+    					listOfTeachers.set(j, teach2);			//swapping students
+    					listOfTeachers.set(j+1, teach1);		//^^^^
+    				}
+    			}
+    		}
+    	}
+    }
+    
+    // 
+    
+    
+
+    
     
     
     
     
     
 }
+    
+    
+    
+    
+    
+    
