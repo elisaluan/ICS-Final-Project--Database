4ef620287b9b287a88c89154167b797b696b75a0
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2018-06-14 18:34:39 -0400

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,17 +1,73 @@
 import java.util.*;
 
-public class Teacher {
+public class Teacher extends User{
    private final int MAX_COURSES = 3;
    private int numTeaching;
-   private ArrayList<String> coursesTeaching = new ArrayList<String> ();
+   private String [] coursesTeaching = new String [MAX_COURSES];
+   private ArrayList <Course> courseArrayList = new ArrayList<Course>();
    private Date beginTeach;
-   // accessors
+
+   // Accessors
    public int getNumTeaching () {
       return numTeaching;
-   } 
-   public Arraylist<String> getCoursesTeaching () {
+   }
+
+   public String[] getCoursesTeaching() {
+
       return coursesTeaching;
    }
-   public Date getBeginTeac
-   // mutators
+
+   public ArrayList<Course> getCourseArrayList() {
+      return courseArrayList;
+   }
+
+   public int getMAX_COURSES() {
+      return MAX_COURSES;
+   }
+
+   public Date getBeginTeach() {
+      return beginTeach;
+   }
+
+   //Mutators
+   public void setNumTeaching(int numTeaching) {
+      this.numTeaching = numTeaching;
+   }
+
+   public void setCoursesTeaching(String[] coursesTeaching) {
+      this.coursesTeaching = coursesTeaching;
+   }
+
+   public void setCourseArrayList(ArrayList<Course> courseArrayList) {
+      this.courseArrayList = courseArrayList;
+   }
+
+   public void setBeginTeach(Date beginTeach) {
+      this.beginTeach = beginTeach;
+   }
+
+   //Method
+
+   public boolean updateAverage (Student other, Course course, String assessmentName, int assessmentMark )
+   {
+         MyCourse temp = other.getTranscript().searchByCourse((course.getCourseCode()));
+
+         for (int i = 0; i < temp.getMarksList().size(); i ++)
+         {
+            if (temp.getMarksList().get(i).getAssessmentName().equalsIgnoreCase(assessmentName))
+            {
+               temp.getMarksList().get(i).setAssessmentNum(assessmentMark);
+               return true;
+            }
+         }
+
+         return false;
+   }
+
+   public void addAssessment (MyAssessment assessment)
+   {
+      
+   }
+   
+
 }
