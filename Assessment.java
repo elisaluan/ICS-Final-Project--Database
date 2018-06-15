0b976f927d1d7a41de4aeae64f153ca240af9f44
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2018-06-15 13:08:08 -0400

    Update Assessment.java

--- Assessment.java
+++ Assessment.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 public class Assessment {
    //creates an ArrayList of MyAssessment
-   ArrayList<MyAssessment> assessmentList = new ArrayList<MyAssessment>();
-   
+   ArrayList<String> assessmentList = new ArrayList<String>();
+   Course forCourse;
+
    // constructor
    // accessor
-   public ArrayList<MyAssessment> getAssessmentList () {
+   public ArrayList<String> getAssessmentList () {
       return assessmentList;
    }
-   
+
    // mutator
-   public void setAssessmentList (ArrayList<MyAssessment> assessmentList) {
+   public void setAssessmentList (ArrayList<String> assessmentList) {
       this.assessmentList = assessmentList;
    }
-   
+
    // method
    public String toString () {
       return "";
