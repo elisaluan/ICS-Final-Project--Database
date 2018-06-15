d188ffc7d94caba4f5ad586c2cfec75a92696757
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2018-06-15 13:01:47 -0400

    Update CoursesByYear.java

--- CoursesByYear.java
+++ CoursesByYear.java
@@ -1,48 +1,20 @@
+import java.util.*;
 public class CoursesByYear {
-	
-	// FIELDS
-	
-	private final int NUM_SEMESTERS = 2;
-	private CoursesBySemester[] semesterList = new CoursesBySemester[NUM_SEMESTERS];
-	
-////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	// CONSTRUCTORS
-	
-	public CoursesByYear(CoursesBySemester[] courses)
-	{
-		for(int i = 0 ; i < NUM_SEMESTERS ; i++)
-		{
-			semesterList[i] = courses[i];
-		}
-	}
-	
-////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	// ACCESSORS
-	
-	public CoursesBySemester [] getSemesterList () {
-	      return semesterList;
-	   }
-	
-////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	// MUTATORS
-	
-	public void setSemesterList (CoursesBySemester[] semesterList) {
-	      this.semesterList = semesterList;
-	   }
-	
-////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	// METHODS
-	
-	public String toString ()
-   {
-      String output = "Semester #1: " + semesterList[0] + "\n//////////////////////////////////\nSemester #2: " 
-              + semesterList[1];
-      return output;
+   private final int NUM_SEMESTERS = 2;
+   private CoursesBySemester[] semesterList = new CoursesBySemester[NUM_SEMESTERS];
+   // constructor
+   // accessor
+   public CoursesBySemester [] getSemesterList () {
+      return semesterList;
    }
+   
+   // mutator
+   public void setSemesterList (CoursesBySemester[] semesterList) {
+      this.semesterList = semesterList;
+   }
+   
+   // method
+
 
    public double calcYearAverage ()
    {
@@ -54,5 +26,12 @@ public class CoursesByYear {
       }
       average = totalSum/NUM_SEMESTERS;
       return average;
+   }
+
+   public String toString ()
+   {
+      String output = "Semester #1: " + semesterList[0] + "\n//////////////////////////////////\nSemester #2: "
+              + semesterList[1];
+      return output;
    }
 }
