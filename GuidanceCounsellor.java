b6439a5ad3a1eeb67293f14167d921712dc5d130
michaelTanson <39774935+michaelTanson@users.noreply.github.com>
2018-06-15 01:48:31 -0400

    Add files via upload

--- GuidanceCounsellor.java
+++ GuidanceCounsellor.java
@@ -1,2 +1,42 @@
-public class GuidanceCounsellor {
+import java.util.*;
+
+public class GuidanceCounsellor extends Teacher {
+	
+	// FIELDS
+	
+	private ArrayList<School> list = AcademicDatabase.getTdsbSchoolList();
+	private FileInputOutput fileIO = AcademicDatabase.getFileIO();
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+
+	// CONSTRUCTOR
+	
+	public GuidanceCounsellor() {
+		super();
+	}
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// METHODS
+	
+	public boolean addStudent(int school, Student stud) {
+		
+		list.get(school).getListOfStudents().add(stud);
+		list.get(school).sortStudentLastAZ();
+		return fileIO.add(stud);
+	}
+	
+	public boolean removeStudent(int school, int idx) {
+		
+		// int lenght = list.size();
+		
+		list.get(school).getListOfStudents().remove(idx);
+		
+//		if(length > list.size()) {
+//			return true;
+//		}else {
+//			return false
+//		}
+	}
+	
 }
